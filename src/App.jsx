48010b8ea69216da6bd850a153507d11978cfe2c
iamelmirr <besirovicelmir36@gmail.com>
2024-12-23 23:53:44 +0100

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Home from "./pages/Home"
 import React, { useState } from "react"
 import LandingPage from "./pages/LandingPage"
+import Authentication from "./pages/Authentication"
 
 function App() {
 const [isAuthenticated, setIsAuthenticated] = useState(false)
+const [isGettingStarted, setIsGettingStarted] = useState(true)
 
   
 
   return (
     <>
-      {!isAuthenticated && <LandingPage></LandingPage>}
+      {isAuthenticated && <LandingPage></LandingPage>}
       {isAuthenticated && <Home></Home>}
+      {isGettingStarted && <Authentication></Authentication>}
     </>
   )
 }
---
+++ src/pages/Authentication.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+export default function Authentication() {
+    return (
+        
+
+        <div className="authentication-body">
+
+            <div className="registration-div">
+                
+            </div>
+
+        </div>
+    )
+}
